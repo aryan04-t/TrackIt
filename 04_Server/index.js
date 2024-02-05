d36eb91a41fd33cd8a4d30054ad542a478daa224
shubhansu-kr <shubhansu2021@gmail.com>
2024-02-05 15:14:01 +0530

    SetUp Initial Server

--- 04_Server/index.js
+++ 04_Server/index.js
@@ -1,96 +1,51 @@
-const express = require("express");          // Import the Express module 
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const path = require('path');
 
+// Configure Env Variables.
+dotenv.config();
+const port = process.env.PORT || 5500;
+const live = process.env.LIVE || 'http://localhost:';
 
-// // Create an instance of Express 
-const app = express(); 
-const port = 3000; 
+// Configure static file path
+const publicDirectoryPath = path.join(__dirname, 'public');
+const filePath = path.join(publicDirectoryPath, 'index.html');
 
+// Create express instance.
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(publicDirectoryPath));
 
-
-// - - - - - - - - - - 
-
-
-
-/* 
-    # Calories and Macros Tracker Section: 
-*/
-
-
-// Defining a route for handling GET requests, fetching all the given data of user for calories and macros 
-app.get('/caloriesAndMacrosTrackerFetchData', (request, response) => { 
-
-    // Send a response back to the client
-    response.send(`Fetched User's Last 30 days stored Data of Calories and Protein`); 
-    response.send(`Showing User the data, if they want to keep data of more than 30 days take subscription`); 
-
+// Serve root Req
+app.get('/', (req, res) => {
+    res.status(200).sendFile(filePath);
 });
 
+const start = async () => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server is listening to port ${port} happily`);
+            console.log(`Go Live at ${live}${port}`);
+        });
+    } catch (err) {
+        console.log('Error starting the server: ', err);
+        process.exit(1);
+    }
+}
 
-// Storing users new given data input for calories and protein 
-app.post('/caloriesAndMacrosTrackerFeedNewData', (request, response) => {
+start();
 
-    // Access the request body for data sent with the PUT request
-    const dataFromClient = request.body;
+// ShutDown on SIGINT signal.
+process.on('SIGINT', () => {
+    console.log('Shutting down gracefully');
 
-    // Perform actions with the data (e.g., create a new resource in the server)
+    // try {
+    //     disconnectDB();
+    // } catch (err) {
+    //     console.log("Error disconnecting mongoDB", err);
+    // }
 
-    // Send a response back to the client 
-    response.send('POST request successful!'); 
-
-}); 
-
-
-
-// - - - - - - - - - - 
-
-
-
-/* 
-    # Notepad: 
-*/
-
-
-// Fetching all notepads of user which are created till this date: 
-app.get('/allNotepadsFetch', (request, response) => {
-
-    response.send('Notepads fetched and displayed successfully');
-
-}); 
-
-
-// Storing users newly created data: 
-app.post('addingNewNotepad', (request, response) => {
-
-    response.send('New Notepad stored successfully'); 
-
+    process.exit(0);
 });
-
-
-
-// - - - - - - - - - - 
-
-
-
-/* 
-    # Expense Tracker: 
-*/
-
-
-
-
-
-
-
-
-// - - - - - - - - - - 
-
-
-
-// Opening Port 
-app.listen(port, () => {
-
-    console.log("Server Successfully Spun Up."); 
-
-}); 
-
-// listen() fuction -> Starts the server on the specified port. The callback function is called once the server is successfully listening, and it logs a message to the console. 
