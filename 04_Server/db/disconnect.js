739d04d0b7a64e0aad2e7915bf9ecaaeac45ae7a
shubhansu-kr <shubhansu2021@gmail.com>
2024-02-05 15:20:38 +0530

    Disconnect db function created

---
+++ 04_Server/db/disconnect.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const disconnectDB = () => {
+    mongoose.connection.close((err) => {
+        if (err) {
+            console.error('Error closing MongoDB connection:', err);
+        } else {
+            console.log('MongoDB connection closed successfully');
+        }
+    });
+};
+
+module.exports = { disconnectDB };
